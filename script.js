eb67558b176f6bb173b70dede2f7be1ba5f7d319
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-10-10 21:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+cy.visit(baseUrl + "/main.html");
+cy.wait(1000); // wait for 1 second to ensure all elements load
+cy.get('form').within(() => {
+    // Other input checks
+    cy.get("select#laptop");
+    cy.get("label[for='laptop']");
+});
